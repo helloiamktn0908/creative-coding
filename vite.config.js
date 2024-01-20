e547fd142c8fdd3599c34d9a92812f88227b9fa3
kotono.ukai <helloiamktn@gmail.com>
2024-01-20 21:02:56 +0900

    first commit

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+const root = resolve(__dirname, "src");
+const outDir = resolve(__dirname, "dist");
+
+export default defineConfig({
+  root,
+  build: {
+    outDir,
+    rollupOptions: {
+      input: {
+        "001": resolve(root, "001", "index.html"),
+      },
+    },
+  },
+});
